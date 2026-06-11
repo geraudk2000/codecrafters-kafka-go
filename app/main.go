ddb48f37a97730e55a178d33a53822358f159cbb
Geraud Kameni <geraudkameni@gmail.om>
2026-06-10 19:38:01 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,6 +14,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
+	//buf := make([]byte, 1024)
 	// TODO: Uncomment the code below to pass the first stage
 	//
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -21,9 +22,16 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	response := []byte{
+		0, 0, 0, 0,
+		0, 0, 0, 7,
+	}
+	conn.Write(response)
+
 }
